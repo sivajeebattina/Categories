abab9bef651f5021cea49835be98497cb776876e
sivajeebattina <sivajee.battina@paradigmcreatives.com>
2015-07-08 12:44:28 +0530

    changes made

--- NSString+RemoveCharacters.h
+++ NSString+RemoveCharacters.h
@@ -10,6 +10,6 @@
 
 @interface NSString (RemoveCharacters)
 
-@property (nonatomic, strong) NSString *inputString;
+@property (nonatomic, strong) NSString *inputStrng;
 -(NSString *)removeNumbersFromString;
 @end
